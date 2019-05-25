b1b099acbd5ba22b69c6469b72b8a23852d89b91
rahalmichel96 <persian1996>
2019-05-25 09:39:27 +0200

    Reste à développer (controler)

--- php/controler/controler.php
+++ php/controler/controler.php
@@ -1,6 +1,7 @@
 <?php
 
-function accueil() {
+function accueil()
+{
 
 }
 
@@ -9,18 +10,15 @@ function signIn()
     require_once('../utils/dbconnect.php');
     if (isset($_SESSION['user'])) {
         $id = $_SESSION['user'];
-        echo json_encode(Utilisateur::getUser($conn,$id),JSON_PRETTY_PRINT);
+        echo json_encode(Utilisateur::getUser($conn, $id), JSON_PRETTY_PRINT);
     }
 
-    echo json_enconde(Utilisateur::signInUser($conn,$_POST['mail'],$_POST['mdp']),JSON_PRETTY_PRINT);
+    echo json_enconde(Utilisateur::signInUser($conn, $_POST['mail'], $_POST['mdp']), JSON_PRETTY_PRINT);
 }
 
 function register()
 {
     require_once('../utils/dbconnect.php');
-    if (count($_POST) == 0) {
-        //TODO
-    }
 
     $prenom = $_POST['prenom'];
     $nom = $_POST['nom'];
@@ -28,12 +26,47 @@ function register()
     $date_naissance = $_POST['date_naisssance'];
     $mdp = $_POST['mdp'];
 
-    echo json_encode(Utilisateur::signUpUser($conn, $prenom, $nom, $mail, $date_naissance, $mdp),JSON_PRETTY_PRINT);
+    echo json_encode(Utilisateur::signUpUser($conn, $prenom, $nom, $mail, $date_naissance, $mdp), JSON_PRETTY_PRINT);
+}
+
+function showProfile()
+{
+
+}
+
+function editProfile() {
+
+}
+
+function createPost() {
+
+}
+
+function showPosts() {
+
+}
+
+function showCandidatesForPost() {
+
+}
+
+function searchPost() {
+
+}
+
+function evaluateLesson() {
+
+}
+
+function reward() {
+
 }
 
 function deconnect()
 {
     session_destroy();
+
+    accueil();
 }
 
 
