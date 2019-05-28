1c9e721eabfe9e64e98c4778253b0ebf14dcdfba
rahalmichel96 <rahalmichel@hotmail.fr>
2019-05-28 18:43:42 +0200

    Merge conflict

--- php/test.php
+++ php/test.php
@@ -5,7 +5,7 @@ ini_set('display_errors',1);
 
 require_once('utils/dbconnect.php');
 require_once('utils/Feedback.php');
-require_once('modele/utilisateur.php');
+require_once('modele/Utilisateur.php');
 require_once('modele/post.php');
 //Utilisateur::signUpUser($conn, "Marie", "Pledosky", "testo@test.test","20/02/1997","12345");
 //Utilisateur::signInUser($conn, "test@test.test", "12345");
