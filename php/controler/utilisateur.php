f9e8eeafb1f5c115cb2acd6346e4d5d9be7785f6
rahalmichel96 <rahalmichel@hotmail.fr>
2019-06-13 16:32:17 +0200

    usage of call_user_func reset for controller utilisateur

--- php/controler/utilisateur.php
+++ php/controler/utilisateur.php
@@ -9,11 +9,11 @@ require_once('../modele/Competence.php');
 require_once('../modele/CompetenceUtilisateur.php');
 require_once("../utils/Feedback.php");
 
-$calledFunction = "showProfile";
-$data = [$conn, 10];
+$calledFunction = $_POST["function"];
+$data = $_POST["data"];
 
-// // Push the $conn to the data
-//array_unshift($data, $conn);
+// Push the $conn to the data
+array_unshift($data, $conn);
 
 //Call the function
 call_user_func_array($calledFunction, $data);
