0d306dc062657d7bdbe68024e9af0ab5a5d683bd
uncyril <cyrilgour@gmail.com>
2019-06-20 20:41:04 +0200

    Oups j'ai encore oublié d'enlever mon jeu de test

--- php/modele/Session.php
+++ php/modele/Session.php
@@ -11,4 +11,39 @@ class Session {
     function __construct() {
         
     }
+
+    /**
+     * STATIC
+     * 
+     * Retourne toutes les sessions d'un utilisateur
+     * 
+     * @param $id = id utilisateur
+     * @return feedback
+     */
+    static function getSessionByUser($conn, $idUser) {
+        $stmt = $conn->prepare("SELECT p.id AS idPost, pseudo, titre, p.description, p.tmp_estime, p.date, p.type, etat
+                                FROM post p, utilisateur u, candidature c
+                                WHERE c.candidat = $idUser
+                                AND p.utilisateur = u.id
+                                AND p.id = c.post");
+        $stmt->execute();
+        $count = $stmt->rowCount();
+        
+        if($count != 0) {
+            
+            $postDetail = $stmt->fetchAll(PDO::FETCH_ASSOC);
+            $cpt = 0;
+            
+            foreach ($postDetail as $row) {
+                $idPost = $postDetail[$cpt]['idPost'];
+
+                $postDetail[$cpt]['tag'] = CompetencePost::getTagPost($conn, $idPost);
+                $cpt++;
+            }
+            
+            return new Feedback($postDetail, true, "");
+        }
+        else
+        return new Feedback(NULL, false, "Aucune candidature.");
+    }
 }
